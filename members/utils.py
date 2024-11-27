e2defd0a599ffa9aea55072a95ac365ca38bd193
MothMalone <nam28042004@gmail.com>
2024-11-27 23:58:54 +0700

    Add functions to retrieve user avatar and validate image URLs.

--- members/utils.py
+++ members/utils.py
@@ -19,6 +19,19 @@ def isAuthenticated(request):
             return {'is_authenticated': False}
     return {'is_authenticated': False}
 
+def getUserAvatar(request):
+    member_id = request.session.get('member_id')
+    avatar_url = None
+
+    if member_id:
+        member = Member.objects.filter(id=member_id).first()
+        if member and member.avatar:
+            avatar_url = member.avatar
+        else:
+            avatar_url = 'https://t4.ftcdn.net/jpg/05/49/98/39/360_F_549983970_bRCkYfk0P6PP5fKbMhZMIb07mCJ6esXL.jpg'
+
+    return {'avatar_url': avatar_url}
+        
 
 def is_password_strong(password, min_length=8):
     contains_special_character = False
@@ -47,3 +60,13 @@ def is_password_strong(password, min_length=8):
         contains_number,
         contains_lowercase_character
     ])
+
+def check_image_url(url):
+    try:
+        response = requests.get(url)
+        # Check if the response is successful and content type is an image
+        if response.status_code == 200 and 'image' in response.headers['Content-Type']:
+            return True
+    except requests.RequestException as e:
+        print(f'Error checking image URL: {e}')
+    return False
