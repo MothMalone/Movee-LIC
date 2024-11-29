85fee5f9c35e782b8266cd174ab7562fda64263a
MothMalone <nam28042004@gmail.com>
2024-11-30 00:47:06 +0700

    Define authorization url patterns

--- members/urls.py
+++ members/urls.py
@@ -9,5 +9,7 @@ urlpatterns  = [
     path('movies/', views.movies, name = 'movies'),
     path('tvseries', views.tvseries, name = 'tvseries'),
     path('tvseries/<int:id>', views.tvseries_detail, name = 'tvseries_detail'),
-    
+    path('signin/', views.signin, name = "signin"),
+    path('signup/', views.signup, name = "signup"),
+    path('logout', views.logout, name = "logout"),
 ]
